06db2ceb13331f5ffb510a528115358f4108d25a
Patrik Lostak <patrik.lostak@gmail.com>
2025-10-27 01:26:47 +0100

    Creation of symtable.c

---
+++ src/symtable.c
@@ -0,0 +1,11 @@
+/**
+ * @file symtable.c
+ * @brief Symtable implementation
+ * @author Patrik Lošťák (xlostap00)
+ * @details This file contains implementation of symtable operations and helper functions
+ *
+ */
+
+ #include "symtable.h"
+
+ 
