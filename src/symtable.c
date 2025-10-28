f29c173232f1bf3e4c637b431f22123308610e51
Patrik Lostak <patrik.lostak@gmail.com>
2025-10-28 01:21:12 +0100

    Finishing implementation of symtable

--- src/symtable.c
+++ src/symtable.c
@@ -30,6 +30,7 @@ size_t symtable_hash(const char *key) {
   return hash % SYMTABLE_DEFAULT_SIZE;
 }
 
+
 ErrorCode symtable_init(SymTable *table) {
   if (table == NULL) {
     return ERR_INTERNAL;
@@ -45,6 +46,7 @@ ErrorCode symtable_init(SymTable *table) {
 
   return ERR_OK;
 }
+
 
 void symtable_free(SymTable *table) {
   if (table == NULL || table->items == NULL) {
@@ -68,6 +70,7 @@ void symtable_free(SymTable *table) {
   table->count = 0;
 }
 
+
 SymbolData *symtable_lookup(SymTable *table, const char *key) {
   if (table == NULL || key == NULL) {
     return NULL;
@@ -88,3 +91,136 @@ SymbolData *symtable_lookup(SymTable *table, const char *key) {
   }
   return NULL; // Not found
 }
+
+
+ErrorCode symtable_insert(SymTable *table, const char *key, SymbolData data) {
+  if (table == NULL || key == NULL) {
+    return ERR_INTERNAL;
+  }
+
+  unsigned index = symtable_hash(key);
+
+  for (unsigned idx = 0; idx < table->size; idx++) {
+    unsigned pos = (index + idx) % table->size;
+    SymItem *item = &table->items[pos];
+
+    if (item->state == SLOT_EMPTY || item->state == SLOT_DELETED) {
+      item->key = strdup(key);
+      if (item->key == NULL) {
+        return ERR_INTERNAL; // Memory allocation failure
+      }
+
+      item->data = data;
+      item->state = SLOT_OCCUPIED;
+      table->count++;
+      return ERR_OK; // Successfully inserted
+    }
+
+    if (item->state == SLOT_OCCUPIED && strcmp(item->key, key) == 0) {
+      return ERR_SEMANTIC_REDEFINITION; // Symbol already exists
+    }
+  }
+
+  return ERR_INTERNAL; // Table full
+}
+
+
+ErrorCode symtable_delete(SymTable *table, const char *key) {
+  if (table == NULL || key == NULL) {
+    return ERR_INTERNAL;
+  }
+
+  unsigned index = symtable_hash(key);
+
+  for (unsigned idx = 0; idx < table->size; idx++) {
+    unsigned pos = (index + idx) % table->size;
+    SymItem *item = &table->items[pos];
+
+    if (item->state == SLOT_EMPTY) {
+      return ERR_SEMANTIC_UNDEFINED; // Not found
+    }
+
+    if (item->state == SLOT_OCCUPIED && strcmp(item->key, key) == 0) {
+      free(item->key);
+      // Free SymbolData if it contains dynamically allocated memory
+      if (item->data.kind == SYM_FUNCTION && item->data.info.function.param_types) {
+        free(item->data.info.function.param_types);
+      }
+
+      item->state = SLOT_DELETED;
+      table->count--;
+      return ERR_OK; // Successfully deleted
+    }
+  }
+
+  return ERR_SEMANTIC_UNDEFINED; // Not found
+}
+
+
+SymbolData symtable_make_variable(DataType type) {
+  SymbolData data;
+  data.kind = SYM_VARIABLE;
+  data.type = type;
+  data.defined = true;
+  return data;
+}
+
+SymbolData symtable_make_function(DataType return_type,size_t param_count) {
+  SymbolData data;
+  data.kind = SYM_FUNCTION;
+  data.type = return_type;
+  data.defined = false; // Functions are initially declared but not defined
+  data.info.function.param_count = param_count;
+
+  if (param_count > 0) {
+    data.info.function.param_types = malloc(param_count * sizeof(DataType));
+    if (data.info.function.param_types == NULL) {
+      // Handle memory allocation failure
+      data.info.function.param_count = 0;
+    }
+
+    for (unsigned idx = 0; idx < param_count; idx++) {
+      data.info.function.param_types[idx] = TYPE_UNDEFINED; // Initialize parameter types
+    }
+
+  } else {
+    data.info.function.param_types = NULL;
+  }
+  return data;
+}
+
+
+SymbolData symtable_make_getter(DataType type) {
+  SymbolData data = symtable_make_function(type, 0);
+  data.kind = SYM_GETTER;
+  data.defined = true;
+  return data;
+}
+
+SymbolData symtable_make_setter(DataType param_type) {
+  SymbolData data = symtable_make_function(TYPE_NULL, 1);
+  data.kind = SYM_SETTER;
+  data.defined = true;
+
+  if (data.info.function.param_types != NULL) {
+    data.info.function.param_types[0] = param_type;
+  }
+
+  return data;
+}
+
+
+char *make_function_key(const char *name, size_t param_count) {
+  // Calculate the length needed for the new key
+  size_t name_len = strlen(name);
+  size_t key_len = name_len + 1 + 20; // 1 for '#' and 20 for param_count (enough for size_t)
+
+  char *key = malloc(key_len);
+  if (key == NULL) {
+    return NULL; // Memory allocation failure
+  }
+
+  snprintf(key, key_len, "%s#%zu", name, param_count);
+  return key;
+}
+
